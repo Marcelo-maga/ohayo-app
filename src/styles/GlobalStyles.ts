cd70e3e1488baa4bea2fcee6df6c592cfc9948f0
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2021-08-12 09:03:04 -0300

    GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  *, button, input {
+    border: 0;
+    outline: 0;
+  }
+`
+
+export default GlobalStyles
