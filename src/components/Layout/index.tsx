ad1aa6023a2a19208c953457bf1d8590385813e7
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2021-08-12 09:32:53 -0300

    Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import { Grid } from "./styles"
+
+
+const Layout: React.FC = () => {
+  return(
+    <Grid />
+  )
+}
+
+export default Layout
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+// HE - Header
+// SB - SideBar
+// CO - Content
+// DE - Details
+
+
+export const Grid = styled.div`
+
+  grid-template-columns: 775px auto auto;
+  grid-template-rows: 105px auto auto;
+
+  grid-area: 
+    'SB CO DE'
+    'SB CO DE'
+    'SB CO DE';
+
+  height: 100vh;
+`;
