0408a9ac44d93ef2f2973c3a3f51fe32d283435e
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-02 12:21:42 -0300

    FormLogin Básico

---
+++ src/components/LoginForm/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const FormLogin = styled.form`
+  display: flex;
+  flex-direction: column;
+
+`
+export const Input = styled.input`
+  border-radius: 15px;
+
+  :hover{
+    cursor: pointer;
+  }
+`
+
+export const Button = styled.button`
+  border-radius: 10px;
+`
