be0fbd08b86c9ea81f29591fbd5c81eae634d30f
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-03 10:02:35 -0300

    Mundaças nas pastas

---
+++ src/Layout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+// HE - Header
+// SB - SideBar
+// CO - Content
+// DE - Details
+
+export const Grid = styled.div`
+
+  grid-template-areas: 
+    'SB CO'
+    'SB CO'
+    'SB CO';
+`
