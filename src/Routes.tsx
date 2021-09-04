369fe2b5e805f76dd49fabc8186d13b7ac1b68bc
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-04 12:48:25 -0300

    Rotas com segurança

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import LoginPage from './pages/Login'
 
+import PrivateRoute from './components/PrivateRoutes'
+import App from './App'
+
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/login" component={LoginPage} />
+        <PrivateRoute path='/' component={App} />
       </Switch>
     </BrowserRouter>
   )
---
+++ src/components/PrivateRoutes.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+import isAuthtenticate from '../api/auth'
+
+interface Props {
+  path: string;
+  component: any;
+}
+
+const PrivateRoute: React.FC<Props> = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest}
+    render={props => isAuthtenticate()
+      ? (
+        <Component {...props} />
+        )
+      : (
+        <Redirect to={{ pathname: '/login' }} />
+        )} />
+  )
+}
+
+export default PrivateRoute
