b0b62459f6f7472e022d8aee27d7da1da90674eb
Vitor Ayala <vitor.ayala@etec.sp.gov.br>
2021-11-09 11:11:13 -0300

    Componente para criar os projetos

--- src/components/CreateNewProject/index.tsx
+++ src/components/CreateNewProject/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import { Container, Content } from './styles'
+import { Input, Button } from '@chakra-ui/react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import api from '../../services/api'
+
+interface IFormInput {
+  projectName: string,
+  projectDesc: string
+}
 
 const CreateNewProject: React.FC = () => {
+  const { register, handleSubmit } = useForm<IFormInput>({
+  })
+
+  const onSubimit: SubmitHandler<IFormInput> = async ({ projectName, projectDesc }: IFormInput) => {
+    await api.post('/newProject', {
+      projectName,
+      projectDesc
+    })
+  }
+
   return (
     <Container>
       <Content>
+
+      <form onSubmit={handleSubmit(onSubimit)}>
+        <Input {...register('projectName')} placeholder={'Project Name: '} name={'projectName'} size='md' />
+        <Input {...register('projectDesc')} placeholder={'Descrição: '} name={'projectDesc'} size='md' />
+        <Button type='submit'>Criar</Button>
+        <Button type='reset'>Cancel</Button>
+      </form>
 
       </Content>
     </Container>
--- src/components/CreateNewProject/styles.ts
+++ src/components/CreateNewProject/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  
+  grid-area: CO;
 `
 
 export const Content = styled.div`
