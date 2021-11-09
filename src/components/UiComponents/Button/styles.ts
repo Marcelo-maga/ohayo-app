a746f4b36c7bc5e95f4d80edbee67ccc51b57f4e
Vitor Ayala <vitor.ayala@etec.sp.gov.br>
2021-11-09 11:38:39 -0300

    Sidebar

--- src/components/UiComponents/Button/styles.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const ButtonArea = styled.div`
-  
-`
--- src/components/UiComponents/Sidebar/index.tsx
+++ src/components/UiComponents/Sidebar/index.tsx
@@ -1,40 +1,17 @@
-import React, { useState } from 'react'
-import { Input, Button } from '@chakra-ui/react'
+import React from 'react'
 import { AddIcon, EditIcon, BellIcon } from '@chakra-ui/icons'
 
 import { Container, Menu } from './styles'
-import Modal from '../Modal'
 
 const Sidebar: React.FC = () => {
-  const [modalOpen, setModalOpen] = useState(false)
-
-  async function createProject (event: Event) {
-    event.preventDefault()
-    console.log('Cheguei')
-  }
-
   return (
-    <>
-    <Modal modalOpen={modalOpen} >
-      <h1>New Project</h1>
-
-      <form onSubmit={() => console.log('dadss')}>
-        <Input placeholder={'Project Name: '} name={'projectName'} size='md' />
-        <Input placeholder={'Descrição: '} name={'projectDesc'} size='md' />
-        <Button onClick={() => createProject}>Criar</Button>
-        <Button type='reset'>Cancel</Button>
-      </form>
-
-    </Modal>
-
     <Container>
       <Menu>
-        <button onClick={() => setModalOpen(true)} ><AddIcon/></button>
+        <button><AddIcon/></button>
         <button><BellIcon/></button>
         <button><EditIcon/></button>
       </Menu>
     </Container>
-    </>
   )
 }
 
