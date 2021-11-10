fce08f8f3cc288f1e305bd4442850fb58ee44fb2
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-11-10 08:36:36 -0300

    Ajsute na página de Dashborda

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Container, DashBoardContainer } from './styles'
 
-import Layout from '../../components/UiComponents/Layout'
 import Cards from '../../components/Cards'
 
 const Dashboard: React.FC = () => {
   return (
-    <Layout>
       <Container>
           <h1 className={'title'}>Dashboard</h1>
           <DashBoardContainer>
             <Cards />
           </DashBoardContainer>
       </Container>
-    </Layout>
   )
 }
 
