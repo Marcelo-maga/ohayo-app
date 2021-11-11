f39e7daa9ec7126e81d0bb4362dbfa1f1d1e07be
Marcelo <marcelo.maga2550@gmail.com>
2021-11-11 13:47:42 -0300

    Novos estilos para o formulario

--- src/components/CreateNewProject/styles.ts
+++ src/components/CreateNewProject/styles.ts
@@ -1,9 +1,65 @@
 import styled from 'styled-components'
+import { colors } from '../../styles/theme'
 
 export const Container = styled.div`
   grid-area: CO;
+
+  width: 100%;
+  height: 100%;
+
 `
 
 export const Content = styled.div`
-  
+  form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    Input {
+      width: 70%;
+      margin-bottom: 10px;
+    }
+
+    .divColors {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      label {
+        font-weight: bold;
+      }
+
+      div {
+        display: flex;
+        flex-direction: row;
+      }
+    }
+
+    .divBtns {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+
+      .btnCreate {
+        width: 100px;
+        
+        margin-top: 10px;
+        margin-right: 20px;
+
+        color: white;
+        background-color: ${colors.purple.lightPurple};
+      }
+
+      .btnCancel {
+        width: 100px;
+        
+        color: white;
+        background-color: ${colors.tertiary};
+        margin-top: 10px;
+
+      }
+
+    }
+  }
 `
