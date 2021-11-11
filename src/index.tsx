964f1635566b29b9bb245b1b09b2d30e3f1f23d3
Marcelo <marcelo.maga2550@gmail.com>
2021-11-11 16:44:21 -0300

    Remoção dos temas e do layout geral

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { DefaultTheme } from './styles/theme'
-import { ColorModeScript } from '@chakra-ui/color-mode'
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <ColorModeScript initialColorMode={DefaultTheme.config.initialColorMode} />
   </React.StrictMode>,
   document.getElementById('root')
 )
