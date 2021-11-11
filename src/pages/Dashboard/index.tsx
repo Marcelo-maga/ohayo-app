1d01318d9326e8ddb4243d9b49fe61c1785101e0
Marcelo <marcelo.maga2550@gmail.com>
2021-11-11 16:45:44 -0300

    Layout volta

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { Container, DashBoardContainer } from './styles'
 
 import Cards from '../../components/Cards'
+import { Layout } from '../../components/UiComponents/Layout'
 
 const Dashboard: React.FC = () => {
   return (
+    <Layout>
       <Container>
           <h1 className={'title'}>Dashboard</h1>
           <DashBoardContainer>
             <Cards />
           </DashBoardContainer>
       </Container>
+    </Layout>
   )
 }
 
