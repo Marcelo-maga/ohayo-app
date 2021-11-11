9a63f3a039b514545a62ebf3a20eee3014777e01
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-11-11 09:10:39 -0300

    Busca os projetos na api

--- src/components/ProjectList/index.tsx
+++ src/components/ProjectList/index.tsx
@@ -1,16 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
 
 import { Projects } from './styles'
 
-const ProjectList: React.FC = () => {
-  useEffect(() => {
+interface IProject {
+  projectId: number,
+  
+  projectName: string,
+  projectDesc: string,
+  projectColor: string,
+  projectTime: string
+}
 
+const ProjectList: React.FC = () => {
+  const [projects, setProjects] = useState<IProject[]>([])
+
+  useEffect(() => {
+    async function getAllProjects() {
+      await api.get<IProject[]>('/getAllProjects/').then(response => {
+        setProjects(response.data)
+      })
+    }
   }, [])
 
   return (
     <Projects>
       <ul>
-
+        {projects.map(project => {
+          return (
+            <li key={project.projectId}>
+              <div color={project.projectColor} />
+              <p>{project.projectName}</p>
+              <p>{project.projectTime} Dias</p>
+            </li>
+          )
+        })}
       </ul>
     </Projects>
   )
