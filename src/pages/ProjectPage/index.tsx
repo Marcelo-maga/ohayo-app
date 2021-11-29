b563c09163356b82a19ce1f6d346aada727760bf
Marcelo <marcelo.maga2550@gmail.com>
2021-11-29 15:41:28 -0300

    Página do projeto

--- src/pages/ProjectPage/index.tsx
+++ src/pages/ProjectPage/index.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
 
 import { useParams } from 'react-router'
-import { Container } from './styles'
-
+import { Container, Header, Content, Content1, Content2 } from './styles'
+import { HourConter } from '../../components/HourCounter'
+import { VscCheck, VscChromeClose } from 'react-icons/vsc'
+import { ToDoList } from '../../components/ToDoList'
 interface ParamsProjectId {
   projectId: string
 }
@@ -12,11 +14,14 @@ interface IProjectTypes {
   id: string
   name: string
   desc: string
+  timeWorked: number
+  complete: boolean
 }
 
 const ProjectPage: React.FC = () => {
   const params: ParamsProjectId = useParams()
 
+  const [completeProject, setcompleteProject] = useState(false)
   const [project, setProject] = useState<IProjectTypes>()
 
   useEffect(() => {
@@ -28,9 +33,46 @@ const ProjectPage: React.FC = () => {
     getProject()
   }, [])
 
+  async function completeProjectFunction () {
+    console.log(completeProject)
+
+    if (!completeProject) {
+      setcompleteProject(true)
+      await api.post(`/completeProject/${params.projectId}`, {
+        completeProject
+      })
+    } else if (completeProject) {
+      setcompleteProject(false)
+      await api.post(`/completeProject/${params.projectId}`, {
+        completeProject
+      })
+    }
+  }
+
   return (
     <Container>
-      <h1 className={'title'} >{project?.name}</h1>
+
+      <Header>
+        <h1 className={'title'} >{project?.name}</h1>
+        <button onClick={completeProjectFunction} className={'button'} >
+          {!completeProject ? <VscCheck/> : <VscChromeClose/>}
+        </button>
+      </Header>
+
+      <Content>
+
+        <Content1>
+          <ToDoList />
+        </Content1>
+
+        <Content2>
+          <HourConter />
+          <h2>Descrição: </h2>
+          <h3>{project?.desc}</h3>
+        </Content2>
+
+      </Content>
+
     </Container>
   )
 }
--- src/pages/ProjectPage/styles.ts
+++ src/pages/ProjectPage/styles.ts
@@ -24,7 +24,81 @@ export const Container = styled.div`
   }
 `
 
-export const Staus = styled.div`
+export const Content = styled.div`
+  display: grid;
+
+  grid-template-areas: 
+    'CO1 CO2';
+
+  width: 100%;
+  height: 100%;
+
+  grid-template-columns: 70% 30%;
+
+`
+
+export const Content1 = styled.div`
+  grid-area: CO1;
+
   display: flex;
   flex-direction: row;
+
+  width: 100%;
+  height: 100%;
+
+
+`
+
+export const Content2 = styled.div`
+  grid-area: CO2;
+
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+
+  flex-direction: column;
+  align-items: center;
+
+  h2{
+    margin-top: 10px;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+  h3 {
+    font-weight: 500;
+  }
+
+`
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-style: solid;
+    border-width: 2px;
+
+    border-radius: 50%;
+
+    width: 40px;
+    height: 40px;
+
+    margin-right: 10px;
+
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
+
+
 `
